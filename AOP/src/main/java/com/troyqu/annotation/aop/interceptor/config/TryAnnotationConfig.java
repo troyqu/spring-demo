177bfa0882bded9175769ac6781fece1d6173af1
qujianfei <qujianfei@hotmail.com>
2021-11-24 15:50:04 +0800

    添加AOP demo代码

--- AOP/src/main/java/com/troyqu/annotation/aop/interceptor/config/TryAnnotationConfig.java
+++ AOP/src/main/java/com/troyqu/annotation/aop/interceptor/config/TryAnnotationConfig.java
@@ -17,5 +17,6 @@ public class TryAnnotationConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         //通过PathPatterns配置interceptor的生效范围，如果不添加默认会拦截所有请求，所以最好结合实际使用情况配置相应的path
         registry.addInterceptor(new WebRequestHandlerInterceptorAdapter(new LoggerInterceptor())).addPathPatterns("/test", "/login");
+//        registry.addInterceptor(new WebRequestHandlerInterceptorAdapter(new LoggerInterceptor())).addPathPatterns("/**", "/test/**");
     }
 }
