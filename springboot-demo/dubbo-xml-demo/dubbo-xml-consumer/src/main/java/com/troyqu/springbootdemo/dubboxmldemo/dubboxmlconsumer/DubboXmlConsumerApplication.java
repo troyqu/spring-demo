cb2a371ff9845e35b7f28484e5ae9b1b8d0bb819
troyqu <252975574@qq.com>
2023-07-13 11:21:55 +0800

    添加依赖

---
+++ springboot-demo/dubbo-xml-demo/dubbo-xml-consumer/src/main/java/com/troyqu/springbootdemo/dubboxmldemo/dubboxmlconsumer/DubboXmlConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.troyqu.springbootdemo.dubboxmldemo.dubboxmlconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboXmlConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboXmlConsumerApplication.class, args);
+    }
+
+}
