732deacb82dd2b9886e9a547e9d3b11c771cf9de
troyqu <252975574@qq.com>
2023-07-14 11:37:05 +0800

    delete AOP project

--- AOP/src/main/java/com/troyqu/annotation/aop/aspectj/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * aspectj 包中展示基于aspectj实现的aop
- *
- * @Author qujianfei
- * @Date 2021/11/11
- * @Time 上午11:53
- */
-package com.troyqu.annotation.aop.aspectj;
--- AOP/src/main/java/com/troyqu/annotation/aop/interceptor/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * 展示基于interceptor和annotation的aop实现
- * @Author qujianfei
- * @Date 2021/11/11
- * @Time 上午11:51
- */
-package com.troyqu.annotation.aop.interceptor;
